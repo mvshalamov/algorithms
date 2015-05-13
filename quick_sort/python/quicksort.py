423fda94e224405c57aa5d674a1f75b67af8a6c2
mvshalamov <mvshalamov@gmail.com>
2015-05-14 00:15:07 +0300

    add func partition

--- quick_sort/python/quicksort.py
+++ quick_sort/python/quicksort.py
@@ -14,6 +14,29 @@ def quicksort(data):
     data = quicksort(less) + pivot + quicksort(more)
     return data
 
+def partition(data, pivot_index, first_index, last_index):
+    pivot_value = data[pivot_index]
+    left_index = first_index
+    right_index = last_index
+
+    print pivot_value
+
+    while True:
+        while left_index <= right_index and data[left_index] < pivot_value:
+            left_index += 1
+
+        while left_index <= right_index and data[right_index] > pivot_value:
+            right_index -= 1
+
+        if right_index <= left_index:
+            break
+
+        data[left_index], data[right_index] = data[right_index], data[left_index]
+
+    return data
+
+
 if __name__ == '__main__':
     data = [1, 5, 3, 7, 2, 9, 6]
     print quicksort(data)
+    print partition(data, 1, 0, len(data) - 1)
