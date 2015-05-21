54f9f0210bdce3d4f9d46729361aed1e71835f09
m.shalamov <m.shalamov@rmabler-co.ru>
2015-05-21 14:48:53 +0300

    binarysearch if empty list

--- bin_search/python/binsearch.py
+++ bin_search/python/binsearch.py
@@ -1,16 +1,18 @@
 def binsearch(alist, item):
     first = 0
     last = len(alist) - 1
-
-    while first <= last:
-        mid = (first + last) // 2
-        if alist[mid] == item:
-            return mid
-        elif item < alist[mid]:
-            last = mid - 1
-        else:
-            first = mid + 1
-    return mid
+ 
+    if alist:
+        while first <= last:
+            mid = (first + last) // 2
+            if alist[mid] == item:
+                return mid
+            elif item < alist[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+        return mid
 
 if __name__ == '__main__':
     print binsearch([5, 6, 7, 8], 7)
+    print binsearch([], 7)
